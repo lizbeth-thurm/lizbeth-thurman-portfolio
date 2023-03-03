1967a27a87ee0c5463d95892da268b197a6bfe52
slthurman01 <slthurman01@gmail.com>
2023-03-03 13:46:14 -0600

    Adds psone css style documents

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,14 +4,12 @@ import Header from "../components/Header";
 import Navigation from "../components/Navigation";
 import Project from "../components/Project";
 
+
 const Home = () => {
   return (
-        <div>
-          <h2>Thurman Portfolio</h2>
-          <p className="text-gray-600">
-            <p>Welcome to my portfolio</p>
-          </p>
-        </div>
+    <div>
+        <h2>Thurman Portfolio</h2>
+    </div>
   );
 };
 
