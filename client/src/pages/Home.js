41e39bfe778f11ef26a3f65dc24f2401675e86e8
slthurman01 <slthurman01@gmail.com>
2023-03-03 13:21:34 -0600

    Sets up initial Home.js file.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+import Project from "../components/Project";
+
+const Home = () => {
+  return (
+        <div>
+          <h2>Thurman Portfolio</h2>
+          <p className="text-gray-600">
+            <p>Welcome to my portfolio</p>
+          </p>
+        </div>
+  );
+};
+
+export default Home;
