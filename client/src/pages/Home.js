eda3150ff7fe5f1b39cd29688bcf598551107bed
slthurman01 <slthurman01@gmail.com>
2023-03-05 21:17:20 -0600

    Adds home page with title and introduction container

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,29 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
 import Project from "../components/Project";
-
+import "../PSOne/PSOne.css";
 
 const Home = () => {
   return (
     <div>
-        <h2>Thurman Portfolio</h2>
+      <h2>Skipper Lizbeth Thurman</h2>
+      <h3>Full-Stack Web Development...and More!</h3>
+      <div class="container">
+        <label class="title">Introduction</label>
+        <p>
+          Welcome to the professional web development and coding portfolio of
+          Skipper Lizbeth Thurman!
+        </p>
+      </div>
+
+      <label>
+  <input type="radio" name="test" value="small" checked>
+  <span class="option">Yes
+    <span class="click"></span>
+  </span>
+  </input>
+</label>
+      
     </div>
   );
 };
